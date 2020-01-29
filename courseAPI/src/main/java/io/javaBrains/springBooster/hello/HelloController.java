8557e44656c50332ccdb8616b644c8c129ef8a93
Partha.Paul <Partha.Paul@ril.com>
2020-01-29 23:42:16 +0530

    Committing

---
+++ courseAPI/src/main/java/io/javaBrains/springBooster/hello/HelloController.java
@@ -0,0 +1,13 @@
+package io.javaBrains.springBooster.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi";
+	}
+}
